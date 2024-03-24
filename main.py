1d1da71b05ddb5e8b6ae6e5ad3b4bda4cef1e2c5
siimgontmacher <164740280+siimgontmacher@users.noreply.github.com>
2024-03-24 17:57:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ template = """
  You are a marketing copywriter with 20 years of experience. You are analyzing customer's background to write personalized product description that only this customer will receive; 
     PRODUCT input text: {content};
     CUSTOMER age group (y): {agegroup};
-    CUSTOMER main Size: {size};
-    TASK: Write a product description that is tailored into this customer's Age group and size. Use age group specific slang.;
+    CUSTOMER main Hobby: {hobby};
+    TASK: Write a product description that is tailored into this customer's Age group and hobby. Use age group specific slang.;
     FORMAT: Present the result in the following order: (PRODUCT DESCRIPTION), (BENEFITS), (USE CASE);
     PRODUCT DESCRIPTION: describe the product in 5 sentences;
-    BENEFITS: describe in 3 sentences why this product is perfect considering customers age group and size;
-    USE CASE: write a story in 5 sentences, of an example working day profession taking into account size {size} and age {agegroup}, write a story in first person, example "I started my workday morning with ...";
+    BENEFITS: describe in 3 sentences why this product is perfect considering customers age group and hobby;
+    USE CASE: write a story in 5 sentences, of an example weekend activity taking into account hobby {hobby} and age {agegroup}, write a story in first person, example "I started my Saturday morning with ...";
 """
 
 prompt = PromptTemplate(
-    input_variables=["agegroup", "size", "content"],
+    input_variables=["agegroup", "hobby", "content"],
     template=template,
 )
 
 def load_LLM(openai_api_key):
     """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(model_name='gpt-3.5-turbo-instruct', temperature=.9, openai_api_key=openai_api_key)
+    llm = OpenAI(model_name='gpt-3.5-turbo-instruct', temperature=.7, openai_api_key=openai_api_key)
     return llm
 
 st.set_page_config(page_title="Customer tailored content", page_icon=":robot:")
@@ -33,12 +33,12 @@ st.header("Personaliseeritud turundusteksti konverter")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("Otstarve: tootetutvustustekstide personaliseerimine igale kliendile või kliendigruppidele; väljundtekst on kohandatud kliendi a) vanuserühmaga ja b) suurusega; sisendtekstiks on neutraalses vormis tootekirjeldus. \
-    \n\n Kasutusjuhend: 1) valmista ette tootekirjeldus (sisendtekst). 2) määra tarbijasegemendid lähtuvalt vanuserühma ja suuruse kombinatsioonidest. 3) sisesta ükshaaval tarbijasegmentide lõikes eeltoodud info äpi kasutajaliideses, saada ära. \
+    st.markdown("Otstarve: tootetutvustustekstide personaliseerimine igale kliendile või kliendigruppidele; väljundtekst on kohandatud kliendi a) vanuserühmaga ja b) hobbitegevusega; sisendtekstiks on neutraalses vormis tootekirjeldus. \
+    \n\n Kasutusjuhend: 1) valmista ette tootekirjeldus (sisendtekst). 2) määra tarbijasegemendid lähtuvalt vanuserühma ja hobbide kombinatsioonidest. 3) sisesta ükshaaval tarbijasegmentide lõikes eeltoodud info äpi kasutajaliideses, saada ära. \
     4) kopeeri ükshaaval tarbijasegmentide lõikes äpi väljundteksti kõnealuse toote tutvustuslehele.")
 
 with col2:
-    st.image(image='companylogo.jpg', caption='Natural and healthy suits for everybody')
+    st.image(image='companylogo.jpg', caption='Natural and healthy shirts for everybody')
 
 st.markdown("## Enter Your Content To Convert")
 
@@ -56,13 +56,13 @@ col1, col2 = st.columns(2)
 with col1:
     option_agegroup = st.selectbox(
         'Which age group would you like your content to target?',
-        ('16-20', '21-30', '31-40', '41-60', '61-80', '81-100'))
+        ('9-15', '16-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-100'))
     
-def get_size():
-    input_text = st.text_input(label="Customers main size", key="size_input")
+def get_hobby():
+    input_text = st.text_input(label="Customers main hobby", key="hobby_input")
     return input_text
 
-size_input = get_size()
+hobby_input = get_hobby()
 
 def get_text():
     input_text = st.text_area(label="Content Input", label_visibility='collapsed', placeholder="Your content...", key="content_input")
@@ -76,7 +76,7 @@ if len(content_input.split(" ")) > 700:
 
 def update_text_with_example():
     print ("in updated")
-    st.session_state.content_input = "suits, all clolors, cotton, responsible manufacturing"
+    st.session_state.content_input = "t shirts, all clolors, cotton, responsible manufacturing"
 
 st.button("*GENERATE TEXT*", type='secondary', help="Click to see an example of the content you will be converting.", on_click=update_text_with_example)
 
